89ae74bbbb84bb5212b247261f7a08d7aa4425f7
PredictiveManish <manisht0914@gmail.com>
2025-01-04 00:09:43 +0530

    Mood setter

---
+++ python/problem5.py
@@ -0,0 +1,13 @@
+# Suggest an activity based on the weather (e.g Sunny- Go for a walk, Rainy- Read a book, Snowy - Build a snowman.)
+
+weather = str(input("How is weather in your surroundings?"))
+
+if weather == 'Sunny':
+    print("Go for a walk.")
+elif weather == 'Rainy':
+    print("Read a book.")
+elif weather=='Snowy':
+    print("Build a snowman")
+else:
+    print("Go and study man, it will give you good life.")
+
