b55aa28cf84747542e79993db7151fa41f93dce8
PredictiveManish <manisht0914@gmail.com>
2025-01-03 23:40:18 +0530

    conditional

---
+++ python/problem2.py
@@ -0,0 +1,16 @@
+# Movie ticket pricing
+
+days = input("Enter the day of week: ")
+age = int(input("Enter your age: "))
+if days=='Wednesday':
+    print("You will get $2 discount.")  
+    if(age<18):
+        print("Your ticket price is $6(Child Original-$8).")
+    else:
+        print("You have to pay $10(Adult Original-12$).")
+else:
+    if(age<18):
+        print("Your ticket price is $8.")
+    else:
+        print("You have to pay $12.")
+    
