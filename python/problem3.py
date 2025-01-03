64692e190bf36c6c1a8f152644b85b4fbf046746
PredictiveManish <manisht0914@gmail.com>
2025-01-03 23:46:01 +0530

    Grade calculator

---
+++ python/problem3.py
@@ -0,0 +1,14 @@
+# Grade Calculator
+marks = int(input("Enter your marks in the subject: "))
+
+if(100<=marks<=90):
+    print("A")
+elif(89<=marks<=80):
+    print("B")
+elif(70<=marks<=79):
+    print("C")
+elif(60<=marks<=69):
+    print("D")
+else:
+    print("F")
+
