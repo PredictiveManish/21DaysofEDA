fcd0a79ae4f9d83c146c14c3fe5d1ee71919aa25
PredictiveManish <manisht0914@gmail.com>
2025-01-04 00:01:01 +0530

    fruit checker

--- python/problem4.py
+++ python/problem4.py
@@ -1,1 +1,14 @@
-# Fruit Ripeness calculator
+# Fruit Ripeness calculator- Determine if a fruit is ripe, overripe, or unique based on its color 
+# (e.g. Banana, Green - Unripe, Yellow-Ripe, Brown-overripe.)
+
+fruit = "Banana"
+fruit_color = str(input("Enter color of fruit: Green/Yellow/Brown. "))
+if fruit_color=='Green':
+    ripeness = 'Unripe'
+elif fruit_color == 'Yellow':
+    ripeness = 'Ripe'
+elif fruit_color=='Brown':
+    ripeness = 'Overripe'
+else:
+    ripeness = "banana, observe your fruit with open eyes!"
+print(f"Your {fruit} is {ripeness}")
