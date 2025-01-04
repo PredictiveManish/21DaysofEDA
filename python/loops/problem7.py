6eed9d7638c7fad04e400a75433e989ee3baa5ca
PredictiveManish <manisht0914@gmail.com>
2025-01-04 19:42:56 +0530

    Checker

--- python/loops/problem7.py
+++ python/loops/problem7.py
@@ -1,1 +1,8 @@
-n
+# Validate input, keep asking user for input until they enter a number between 1 and 10
+while True:
+    number = int(input("Enter any number b/w 1 and 10: "))
+    if 1<=number<=10:
+        print("Continue")
+    else:
+        print("Try again!")
+    
